0b509e6dc7cf7d9b982cbd2bc455e843c37efcb7
Nickraai <92366205+Nickraai@users.noreply.github.com>
2022-03-07 00:53:31 +0000

    plaatjes van producten toegevoegd , producten aangepast , extra table geadd , api bewerkt zodat het ram laat zien

--- db/create.sql
+++ db/create.sql
@@ -8,18 +8,22 @@ CREATE TABLE products (
   name VARCHAR(255),
   description TEXT,
   price NUMERIC(10, 2),
-  videokaarten_id integer
+  ram NUMERIC(10),
+  videokaarten_id INTEGER,
+  
+  cpu_id INTEGER
 );
   
 CREATE TABLE videokaarten (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(255)
   );
-  
-CREATE TABLE ramaantal (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+ CREATE TABLE cpu (
+ id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(255)
   );
+  
 --
 -- populate with data
 --
@@ -29,18 +33,21 @@ CREATE TABLE ramaantal (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
- insert into products (name, description, code, price,videokaarten_id) values ('Macbook Pro', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id.', '816905633-0', 10.5,1);
- insert into products (name, description, code, price,videokaarten_id) values ('Post Mortem', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11,3);
- insert into products (name, description, code, price,videokaarten_id) values ('Scarlet and the Black, The', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5,1);
- insert into products (name, description, code, price,videokaarten_id) values ('Aquí llega Condemor, el pecador de la pradera', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5,2);
- insert into products (name, description, code, price,videokaarten_id) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14,2);
- insert into products (name, description, code, price,videokaarten_id) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14,1);
+ insert into products (name, description, code, price,ram,videokaarten_id) values ('Apple iMac 27-inch 2020','Een goede algemene alles in 1 computer' , '816905633-0', 1900,128,2);
+ insert into products (name, description, code, price,ram,videokaarten_id) values ('Alienware Aurora R11', 'Een echte powerhouse computer voor gemakkelijk en soepel gamen', '077030122-3', 2000,128,1);
+ insert into products (name, description, code, price,ram,videokaarten_id) values ('Dell G5 Gaming Desktop 5090', 'De beste betaalbare gaming computer', '445924201-X', 1000,64,1);
+ insert into products (name, description, code, price,ram,videokaarten_id) values ('Microsoft Surface Studio 2', 'De beste computer for artiesten en fotografen', '693155505-7', 3500,32,1);
+ insert into products (name, description, code, price,ram,videokaarten_id) values ('MSI MEG Trident X', 'Een prachtige computer met een mooi design en krachtige performance', '686928463-6',2449,64,1);
+ insert into products (name, description, code, price,ram,videokaarten_id) values ('iBuypower Revolt 3 i7BG', 'Beste kleine gaming computer', '492662523-7', 2600,16,1);
+  insert into products (name, description, code, price,ram,videokaarten_id) values ('Corsair One i300', 'Een van de beste compacte high-end gaming computers', '492662523-7', 4600,64,1);
+   insert into products (name, description, code, price,ram,videokaarten_id) values ('HP Omen 30L', 'Een computer met aantrekkelijk design en sterke componenten', '492662523-7', 2200,32,1);
+  insert into products (name, description, code, price,ram,videokaarten_id) values ('Razer Blade 15', 'De beste gaming laptop', '492662523-7', 2500,32,1);
+    insert into products (name, description, code, price,ram,videokaarten_id) values ('Asus ROG Zephyrus G14', 'De beste laptop voor rauwe processing kracht', '492662523-7', 1300,16,1);
+  
 
- insert into videokaarten (name) values ('Nvidia');
+  insert into videokaarten (name) values ('Nvidia');
   insert into videokaarten (name) values ('AMD');
   insert into videokaarten (name) values ('Intel');
 
-  insert into ramaantal (name) values ('6');
-  insert into ramaantal (name) values ('8');
-  insert into ramaantal (name) values ('16');
-  insert into ramaantal (name) values ('32');
+  insert into cpu (name) values ('AMD');
+  insert into cpu (name) values ('Intel');
